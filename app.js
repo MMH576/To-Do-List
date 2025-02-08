e497e7b5977c2a0895d9c33266e33132a80636dc
MMH576 <monjorulmohammed@gmail.com>
2025-02-08 16:58:00 -0500

    made function to add new div with 2 buttons inside it

---
+++ app.js
@@ -0,0 +1,33 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event listeners
+todoButton.addEventListener('click', addTodo);
+
+//Functions
+function addTodo(event){
+    //prevent form from submitting
+    event.preventDefault();
+    //create todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'Hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //CHECKED button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    //CHECKED button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('complete-btn');
+    todoDiv.appendChild(trashButton);
+    //Append to list
+    todoList.appendChild(todoDiv);
+}
