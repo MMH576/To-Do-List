21e0e91b3fba0035d872b360eef9d2eecee756a2
MMH576 <monjorulmohammed@gmail.com>
2025-02-08 23:01:17 -0500

    added select options ot html and functions for it in js and other minor changes

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filteroption = document.querySelector('.filter-todo');
 
 //Event listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filteroption.addEventListener('click', filterTodo)
 
 //Functions
 function addTodo(event){
@@ -40,7 +42,11 @@ function deleteCheck(e){
     //DELETE TODO
     if(item.classList[0] === "trash-btn"){
         const todo = item.parentElement;
-        todo.remove();
+        //ANIMATION
+        todo.classList.add("fall");
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        })
     }
 
     //CHECK TODO
@@ -48,4 +54,29 @@ function deleteCheck(e){
         const todo = item.parentElement;
         todo.classList.toggle("completed");
     }
+}
+
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = 'flex';
+                break;
+            case "completed":
+                if(todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                }else{
+                    todo.style.display = 'none';
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains("completed")){
+                    todo.style.display = 'flex';
+                } else{
+                    todo.style.display = 'none';
+                }
+                break;
+        }
+    })
 }
